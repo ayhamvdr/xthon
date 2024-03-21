0c041f292a3b36e3f113a8bcdbf2bb4d9e03e627
CoDeRiraq1 <160813055+CoDeRiraq1@users.noreply.github.com>
2024-03-21 19:06:50 +0300

    Update التاك.py

--- zira/plugins/التاك.py
+++ zira/plugins/التاك.py
@@ -1,4 +1,4 @@
-#𝙕𝙚𝙙𝙏𝙝𝙤𝙣 ®
+#mixthon ®
 import asyncio
 import time
 import io
@@ -20,7 +20,7 @@ from telethon.tl import functions
 
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from . import zedub
+from . import mixub
 
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
@@ -33,7 +33,7 @@ plugin_category = "الادمن"
 
 
 moment_worker = []
-@zedub.zed_cmd(pattern="all?(.*)")
+@mixub.mix_cmd(pattern="all?(.*)")
 async def tagall(event):
   global moment_worker
   if event.is_private:
@@ -54,14 +54,14 @@ async def tagall(event):
     moment_worker.append(event.chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in zedub.iter_participants(event.chat_id):
+    async for usr in mixub.iter_participants(event.chat_id):
       usrnum += 1
       usrtxt += f"- [{usr.first_name}](tg://user?id={usr.id}) "
       if event.chat_id not in moment_worker:
         await edit_or_reply(event, "**⎉╎تم إيقـاف التـاك .. بنجـاح ✓**")
         return
       if usrnum == 5:
-        await zedub.send_message(event.chat_id, f"{usrtxt}\n\n- {msg}")
+        await mixub.send_message(event.chat_id, f"{usrtxt}\n\n- {msg}")
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
@@ -69,21 +69,21 @@ async def tagall(event):
     moment_worker.append(event.chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in zedub.iter_participants(event.chat_id):
+    async for usr in mixub.iter_participants(event.chat_id):
       usrnum += 1
       usrtxt += f"- [{usr.first_name}](tg://user?id={usr.id}) "
       if event.chat_id not in moment_worker:
         await edit_or_reply(event, "**⎉╎تم إيقـاف التـاك .. بنجـاح ✓**")
         return
       if usrnum == 5:
-        await zedub.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await mixub.send_message(event.chat_id, usrtxt, reply_to=msg)
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
 
 
 
-@zedub.zed_cmd(pattern="ايقاف التاك?(.*)")
+@mixub.mix_cmd(pattern="ايقاف التاك?(.*)")
 async def stop_tagall(event):
   if not event.chat_id in moment_worker:
     return await edit_or_reply(event, '**- عـذراً .. لا يوجـد هنـاك تـاك لـ إيقـافـه ؟!**')
@@ -95,7 +95,7 @@ async def stop_tagall(event):
     return await edit_or_reply(event, '**⎉╎تم إيقـاف التـاك .. بنجـاح ✓**')
 
 
-@zedub.zed_cmd(pattern="تاك(?:\s|$)([\s\S]*)")
+@mixub.mix_cmd(pattern="تاك(?:\s|$)([\s\S]*)")
 async def tagall(event):
   global moment_worker
   if event.is_private:
@@ -109,21 +109,21 @@ async def tagall(event):
     if msg == None:
         return await edit_or_reply(event, "**- عـذراً ... الرسـالة غيـر ظـاهـرة للأعضـاء الجـدد ؟!**")
   elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await edit_or_reply(event, "**- اضـف نـص لـ الامـر . . .**\n\n**- مثـال :** `.all وينكـم`")
+    return await edit_or_reply(event, "**- اضـف نـص لـ الامـر . . .**\n\n**- مثـال :** `.all ? وينكـم`")
   else:
     return await edit_or_reply(event, "**- بالـرد عـلى رسـالـه . . او باضـافة نـص مـع الامـر**")
   if mode == "by_cmd":
     moment_worker.append(event.chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in zedub.iter_participants(event.chat_id):
+    async for usr in mixub.iter_participants(event.chat_id):
       usrnum += 1
       usrtxt += f"- [{usr.first_name}](tg://user?id={usr.id}) "
       if event.chat_id not in moment_worker:
         await edit_or_reply(event, "**⎉╎تم إيقـاف التـاك .. بنجـاح ✓**")
         return
       if usrnum == 5:
-        await zedub.send_message(event.chat_id, f"{usrtxt}\n\n- {msg}")
+        await mixub.send_message(event.chat_id, f"{usrtxt}\n\n- {msg}")
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
@@ -131,20 +131,20 @@ async def tagall(event):
     moment_worker.append(event.chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in zedub.iter_participants(event.chat_id):
+    async for usr in mixub.iter_participants(event.chat_id):
       usrnum += 1
       usrtxt += f"- [{usr.first_name}](tg://user?id={usr.id}) "
       if event.chat_id not in moment_worker:
         await edit_or_reply(event, "**⎉╎تم إيقـاف التـاك .. بنجـاح ✓**")
         return
       if usrnum == 5:
-        await zedub.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await mixub.send_message(event.chat_id, usrtxt, reply_to=msg)
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
 
 
-@zedub.zed_cmd(pattern="تبليغ$")
+@mixub.mix_cmd(pattern="تبليغ$")
 async def _(event):
     mentions = "- انتباه الى المشرفين تم تبليغكم \n@admin"
     chat = await event.get_input_chat()
@@ -158,7 +158,7 @@ async def _(event):
     await event.delete()
 
 
-@zedub.zed_cmd(
+@mixub.mix_cmd(
     pattern="منشن(?:\s|$)([\s\S]*)",
     command=("منشن", plugin_category),
     info={
